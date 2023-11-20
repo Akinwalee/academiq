92542e22148c746ce8bdac3dae442fb5f0ca137e
Akinwale <obatulafuad@gmail.com>
2023-11-20 09:31:47 +0100

    Implemented auth routes

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-//APP Main Entry Point
+//Main Entry Point
 
+const express = reuire("express");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+
+//Use Express JSON Middleware
+app.use(express.json())
 
 //Establlish a connection to the database
 mongoose.connect("mongodb://localhost/", {
@@ -13,3 +18,12 @@ mongoose.connect("mongodb://localhost/", {
     .catch((error) => {
         console.error("Error connecting to database:", error.message)
     })
+
+//Routes
+app.use("auth/", authRoutes);
+
+//Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
+//Register a new user
 router.post("/register", async (req, res) => {
     try{
         const {name, username, email, password} = req.body;
         const newUser = new User({name, username, email, password});
         await newUser.save();
-        res.status(201).send("User registered successfully");
+        res.status(201).json({message: "User registered successfully"});
     } catch(error) {
-        res.status(500).send("Error registering user");
+        res.status(500).json({message: "Error registering user"});
+    }
+});
+
+//Login
+router.post("/login", async (req, res) => {
+    try{
+        const {name, username, email, password} = req.body;
+        const user = await User.findOne({username});
+        if (!user) {
+            return (res.status(404).json({message: "User was not found"}));
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return (res.status(401).json({message: "Invalid password"}));
+        }
+        res.status(200).json({message: "Login successful"});
+    } catch (error) {
+        res.status(500).json({message: "Error logging in"});
     }
 });
 
