0ffb24291bfe50fe52adfa09c0310dc0b84e7134
Akinwale <obatulafuad@gmail.com>
2023-11-23 11:01:16 +0100

    Update question.js

--- models/Question.js
+++ models/Question.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const QuestionSchema = new mongoose.Schema({
+    //Add more fields to the schema if needed
     questionText: {type: String, required: true},
     options: [{type: String, required: false}],
     correctAnswer: {type: String, required: true},
